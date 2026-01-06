57b177ed0e06e988b0fd2ff043e7d08da244e58e
Lynn <1060728903@qq.com>
2026-01-06 10:45:19 +0800

    用while循环解决小精灵爬塔的问题

---
+++ 2-ExerciseForCPP/2.4-LoopStructure/EscapeFMT/cmake-build-debug/CMakeFiles/EscapeFMT.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EscapeFMT.dir/main.c.obj"
+  "EscapeFMT.exe"
+  "EscapeFMT.exe.manifest"
+  "EscapeFMT.pdb"
+  "libEscapeFMT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/EscapeFMT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 2-ExerciseForCPP/2.4-LoopStructure/EscapeFMT/cmake-build-debug/CMakeFiles/EscapeFMT.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EscapeFMT.
+# This may be replaced when dependencies are built.
