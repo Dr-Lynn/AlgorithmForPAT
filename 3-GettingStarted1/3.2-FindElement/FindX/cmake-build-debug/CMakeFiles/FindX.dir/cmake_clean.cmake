f0716d03bec5efaba2d9c7fed4c8cfa5f06dd149
Lynn <1060728903@qq.com>
2026-01-13 18:47:49 +0800

    3.1可算是做完了

---
+++ 3-GettingStarted1/3.2-FindElement/FindX/cmake-build-debug/CMakeFiles/FindX.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FindX.dir/main.cpp.obj"
+  "FindX.exe"
+  "FindX.exe.manifest"
+  "FindX.pdb"
+  "libFindX.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FindX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
